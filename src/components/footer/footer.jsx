bcf009e776fd58b0151bc9621ec02678cfe4ce8c
Sasha <sasha@v4scale.com>
2023-08-31 18:43:47 +0300

    added routing

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer>
+    <h3>Created for course webDev</h3>
+  </footer>
+);
+
+export default Footer;
--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,24 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
+import LatestNews from "./pages/latest-news/latest-news";
+import PopularNews from "./pages/popular-news/popular-news";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="latest-news" element={<LatestNews />} />
+            <Route path="popular-news" element={<PopularNews />} />
+          </Routes>
+        </App>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
